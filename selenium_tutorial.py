7c12b040d048d23e34b4740d2c457fcd94e06958
Muhammed Ali <muhamzyali@gmail.com>
2021-06-03 12:35:44 +0100

    some more code

--- selenium_tutorial.py
+++ selenium_tutorial.py
@@ -1,4 +1,4 @@
-
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -9,34 +9,46 @@ driver = webdriver.Chrome('/home/muhammed/Desktop/dev/blog-repo/selenium-tutoria
 
 def get_url(search_text):
     """Generate a url from search text"""
-    template = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss_1'.format(search_text)
-    return template
+    url = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss_1'.format(search_text)
+    
+    # add page query
+    url += '&page{}'
+        
+    return url
 
 
-url = get_url('sneakers')
-driver.get(url)
-
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-record = soup.find_all('div', {'data-component-type': 's-search-result'})
-single_record = record[0]
-# print(single_record)
-
-
-
-def extract_record(item):
+def extract_record(single_record):
     """Extract and return data from a single record"""
+
     
     # Title
     title_tag = single_record.h2.a
     title = title_tag.text.strip()
     url = 'https://www.amazon.com' + title_tag.get('href')
     
-    # product price
-    price_parent = single_record.find('span', 'a-price')
-    price = price_parent.find('span', 'a-offscreen').text
+    # because some products dont have prices we have to 
+    # use try-except block to catch AttributeError
+    try:
+        # product price
+        price_parent = single_record.find('span', 'a-price')
+        price = price_parent.find('span', 'a-offscreen').text
+    except AttributeError:
+        return
+
     result = (title, price)
     
     return result
+
+
+
+
+
+
+
+
+
+
+
 
 
 records = []
@@ -47,3 +59,5 @@ results = soup.find_all('div', {'data-component-type': 's-search-result'})
 # extract data from each results
 for item in results:
     records.append(extract_record(item))
+
+print(item)
